10b3ead0de75c2cb1f1346e5d5ce8acacd45d62b
Antony Chiramel <antonychiramel07@gmail.com>
2021-04-15 23:54:38 +0530

    initial commit

---
+++ precache-manifest.1fa8a48c82a0f397b5145f5336637b10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c7b10b8aaeb894f0ccb9372fba26c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01c0a42f123bc2f6c734",
+    "url": "/static/css/main.b3d46077.chunk.css"
+  },
+  {
+    "revision": "a37b61711c23dbf13a60",
+    "url": "/static/js/2.efbd6ee5.chunk.js"
+  },
+  {
+    "revision": "d50cbe733d7e1e83d34da7158551fa9c",
+    "url": "/static/js/2.efbd6ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c0a42f123bc2f6c734",
+    "url": "/static/js/main.dacd90b3.chunk.js"
+  },
+  {
+    "revision": "864964386b8fbd0a16b5",
+    "url": "/static/js/runtime-main.0fe5df00.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/static/media/preloader.c114fda3.gif"
+  }
+]);
